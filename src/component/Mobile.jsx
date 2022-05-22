8eac656a6c1306ec8cda28f73fc4bdbe5ba14644
vishal1106 <101567849+vishal1106@users.noreply.github.com>
2022-05-22 11:39:33 +0530

    assignment

---
+++ src/component/Mobile.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Mobile = () => {
+  return (
+    <div>
+     <h1>Mobile Operating System</h1>
+     <li>Android</li>
+     <li>Blackberry</li>
+     <li>iphone</li>
+     <li>Windows phone</li>
+     <br />
+     <br />
+     <h1>Mobile Manufacturers</h1>
+     <li>samsung</li>
+     <li>HTC</li>
+     <li>Micromax</li>
+     <li>Apple</li>
+      </div>
+  )
+}
+
+export default Mobile
---
+++ src/component/Navbar.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import "./Navbar.css"
+const Navbar = () => {
+  return (
+     
+    <div className='main'>
+        <div>
+            <h3>LOGOBAKERY</h3>
+        </div>
+        <div className='main1'>
+            <p>service</p>
+            <p>Project</p>
+            <p>About</p>
+
+        </div>
+        <div><button>Contact</button></div>
+
+    </div>
+  )
+}
+
+export default Navbar
